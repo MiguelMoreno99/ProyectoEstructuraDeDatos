ccc281799544dfcfde273c01c9bea14eeeab0c77
Miguel Moreno <mmd.-@hotmail.com>
2022-09-14 00:12:49 -0500

    Se agregaron mas opciones en el menu. Proyecto en proceso para que funcionen las ventanas modales

--- PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
+++ PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
@@ -2,12 +2,14 @@
 #include "resource.h"
 
 BOOL CALLBACK cMenuPrincipal(HWND, UINT, WPARAM, LPARAM);
-BOOL CALLBACK cMenuOpciones(HWND,UINT,WPARAM,LPARAM);
+BOOL CALLBACK cInicioSesion(HWND, UINT, WPARAM, LPARAM);
+HWND hMenuPrincial;
+HMENU hMenuOpciones;
 
 int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdLine, int cShow) {
 
-	HWND hMenuPrincial = CreateDialog(hInst, MAKEINTRESOURCE(DLG_MENUPRINCIPAL), NULL, cMenuPrincipal);
-	HMENU hMenuOpciones = LoadMenu(hInst, MAKEINTRESOURCE(IDR_MENU1));
+	hMenuPrincial = CreateDialog(hInst, MAKEINTRESOURCE(DLG_MENUPRINCIPAL), NULL, cMenuPrincipal);
+	hMenuOpciones = LoadMenu(hInst, MAKEINTRESOURCE(IDR_MENU1));
 	SetMenu(hMenuPrincial, hMenuOpciones);
 
 	MSG msg;
@@ -25,11 +27,25 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 
 	BOOL CALLBACK cMenuPrincipal(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
-		switch (msg){
-		case WM_CREATE: {
+		switch (msg) {
+
+		case WM_INITDIALOG: {
+			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(DLG_INICIOSESION), hwnd, cInicioSesion);
 			break;
 		}
 
+		case WM_COMMAND: {
+
+			switch (LOWORD(wParam)) {
+			case MENU_REGISTRO_PERSONAS:
+				MessageBox(hwnd, L"Comando: Prueba", L"Mensaje de menú", MB_OK);
+				break;
+			case MENU_SISTEMA_SALIR:
+				PostQuitMessage(0);
+				break;
+			}
+			break;
+		}
 		case	WM_CLOSE: {
 			DestroyWindow(hwnd);
 			break;
@@ -39,6 +55,46 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 			break;
 		}
 
+		}
+		return FALSE;
 	}
-	return FALSE;
-}
+
+	BOOL CALLBACK cInicioSesion(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+		switch (msg) {
+
+		case WM_INITDIALOG: {
+			SetFocus(GetDlgItem(hwnd, BTN_INGRESAR));
+			break;
+		}
+
+		case WM_CREATE: {
+
+			break;
+		}
+
+		case WM_COMMAND: {
+
+			switch (LOWORD(wParam)) {
+			case BTN_INGRESAR:
+				EnableWindow(hMenuPrincial, 1);
+				EndDialog(hwnd, BTN_INGRESAR);
+				break;
+			case BTN_SALIR:
+				PostQuitMessage(0);
+				break;
+			}
+			break;
+		}
+		case	WM_CLOSE: {
+			DestroyWindow(hwnd);
+			break;
+		}
+		case WM_DESTROY: {
+			PostQuitMessage(1);
+			break;
+		}
+
+		}
+		return FALSE;
+	}
--- PROYECTO_ESTRUCTURA_DE_DATOS/resource.h
+++ PROYECTO_ESTRUCTURA_DE_DATOS/resource.h
@@ -10,15 +10,24 @@
 #define BTN_INGRESAR                    1003
 #define BTN_SALIR                       1004
 #define BTN_REGISTRARSE                 1005
-#define MENU_PAGINA1_ELEMENTO1          40002
-#define ID_PAGINA1_ELEMENTO2            40003
+#define MENU_REGISTRO_VACUNAS           40017
+#define MENU_REGISTRO_PERSONAS          40018
+#define MENU_REGISTRO_CARNET            40019
+#define MENU_REPORTE_VACUNAS            40020
+#define MENU_REPORTE_PERSONAS           40021
+#define MENU_REPORTE_CARNETS            40022
+#define MENU_REGISTRO_CARNETS           40023
+#define MENU_AYUDA_MANUAL               40024
+#define MENU_SISTEMA_USUARIOS           40025
+#define MENU_SISTEMA_CERRARSESION       40026
+#define MENU_SISTEMA_SALIR              40027
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_COMMAND_VALUE         40028
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
