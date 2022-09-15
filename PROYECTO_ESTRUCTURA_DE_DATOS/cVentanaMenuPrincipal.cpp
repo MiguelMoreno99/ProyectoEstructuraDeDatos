a04b5bff91cd84736a28c0aa4e6e63dd52295492
Miguel Moreno <mmd.-@hotmail.com>
2022-09-15 01:35:15 -0500

    Se completó el diseño de los dialogos de Inicio Sesion, Menu y Captura de Personas, pte que pueda guardar inforamcion el formulario de Captura de personas

--- PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
+++ PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
@@ -3,8 +3,10 @@
 
 BOOL CALLBACK cVentanaPrincipal(HWND, UINT, WPARAM, LPARAM);
 BOOL CALLBACK cInicioSesion(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK cRegistroPersonas(HWND, UINT, WPARAM, LPARAM);
 HWND hVentanaPrincial;
 HWND hInicioSesion;
+HWND hRegistroPersonas;
 HMENU hMenuOpciones;
 
 int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdLine, int cShow) {
@@ -27,11 +29,14 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 		switch (msg) {
 			case WM_INITDIALOG: {
 				hInicioSesion = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(DLG_INICIOSESION), hVentanaPrincial, cInicioSesion);
+				hRegistroPersonas = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(DLG_REGISTROPERSONAS), hVentanaPrincial, cRegistroPersonas);
 				ShowWindow(hInicioSesion, SW_SHOW);
 			}break;
 			case WM_COMMAND: {
 				switch (LOWORD(wParam)) {
 					case MENU_REGISTRO_PERSONAS: {
+						ShowWindow(hwnd, SW_HIDE);
+						ShowWindow(hRegistroPersonas, SW_SHOW);
 					}break;
 					case MENU_SISTEMA_SALIR: {
 						PostQuitMessage(0);
@@ -55,15 +60,15 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 	BOOL CALLBACK cInicioSesion(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		switch (msg) {
 			case WM_INITDIALOG: {
-				SetFocus(GetDlgItem(hwnd, BTN_INGRESAR));
+				SetFocus(GetDlgItem(hwnd, BTN_INICIOSESION_INGRESAR));
 			}break;
 			case WM_COMMAND: {
 				switch (LOWORD(wParam)) {
-					case BTN_INGRESAR: {
+					case BTN_INICIOSESION_INGRESAR: {
 						ShowWindow(hInicioSesion, SW_HIDE);
 						ShowWindow(hVentanaPrincial, SW_SHOW);
 					}break;
-					case BTN_SALIR: {
+					case BTN_INICIOSESION_SALIR: {
 						DestroyWindow(hwnd);
 					}break;
 				}
@@ -74,6 +79,30 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 			case WM_DESTROY: {
 				PostQuitMessage(0);
 			}break;
+		}
+		return FALSE;
+	}
+
+	BOOL CALLBACK cRegistroPersonas(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+		switch (msg) {
+		case WM_INITDIALOG: {
+			SetFocus(GetDlgItem(hwnd, BTN_PERSONA_CAPTURAR));
+		}break;
+		case WM_COMMAND: {
+			switch (LOWORD(wParam)) {
+			case BTN_PERSONA_REGRESAR: {
+				ShowWindow(hwnd, SW_HIDE);
+				ShowWindow(hVentanaPrincial, SW_SHOW);
+			}break;
+			case BTN_PERSONA_CAPTURAR: {
+
+			}break;
+			}
+		}break;
+		case WM_CLOSE: {
+			ShowWindow(hwnd, SW_HIDE);
+			ShowWindow(hVentanaPrincial, SW_SHOW);
+		}break;
 		}
 		return FALSE;
 	}
