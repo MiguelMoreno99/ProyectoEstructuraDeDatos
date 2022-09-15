1a13c87aac7a95a8fa29cbcfd0b56fe9bb7b981b
Miguel Moreno <mmd.-@hotmail.com>
2022-09-14 20:28:50 -0500

    Se acomodaron los case y se va a testear el dialogo modal

--- PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
+++ PROYECTO_ESTRUCTURA_DE_DATOS/cVentanaMenuPrincipal.cpp
@@ -29,31 +29,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 
 		switch (msg) {
 
-		case WM_INITDIALOG: {
-			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(DLG_INICIOSESION), hwnd, cInicioSesion);
-			break;
-		}
-
-		case WM_COMMAND: {
-
-			switch (LOWORD(wParam)) {
-			case MENU_REGISTRO_PERSONAS:
-				MessageBox(hwnd, L"Comando: Prueba", L"Mensaje de menú", MB_OK);
-				break;
-			case MENU_SISTEMA_SALIR:
-				PostQuitMessage(0);
+			case WM_INITDIALOG: {
+				//DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(DLG_INICIOSESION), hwnd, cInicioSesion);
 				break;
 			}
-			break;
-		}
-		case	WM_CLOSE: {
-			DestroyWindow(hwnd);
-			break;
-		}
-		case WM_DESTROY: {
-			PostQuitMessage(1);
-			break;
-		}
+
+			case WM_COMMAND: {
+
+				switch (LOWORD(wParam)) {
+					case MENU_REGISTRO_PERSONAS: {
+						MessageBox(hwnd, L"Comando: Prueba", L"Mensaje de menú", MB_OK);
+						break;
+					}
+					case MENU_SISTEMA_SALIR: {
+						PostQuitMessage(0);
+						break;
+					}
+				}
+				break;
+			}
+			case	WM_CLOSE: {
+				DestroyWindow(hwnd);
+				break;
+			}
+			case WM_DESTROY: {
+				PostQuitMessage(1);
+				break;
+			}
 
 		}
 		return FALSE;
@@ -62,38 +64,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrev, _In_ PSTR cmdL
 	BOOL CALLBACK cInicioSesion(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
 		switch (msg) {
-
-		case WM_INITDIALOG: {
-			SetFocus(GetDlgItem(hwnd, BTN_INGRESAR));
-			break;
-		}
-
-		case WM_CREATE: {
-
-			break;
-		}
-
-		case WM_COMMAND: {
-
-			switch (LOWORD(wParam)) {
-			case BTN_INGRESAR:
-				EnableWindow(hMenuPrincial, 1);
-				EndDialog(hwnd, BTN_INGRESAR);
-				break;
-			case BTN_SALIR:
-				PostQuitMessage(0);
+			case WM_INITDIALOG: {
+				SetFocus(GetDlgItem(hwnd, BTN_INGRESAR));
 				break;
 			}
-			break;
-		}
-		case	WM_CLOSE: {
-			DestroyWindow(hwnd);
-			break;
-		}
-		case WM_DESTROY: {
-			PostQuitMessage(1);
-			break;
-		}
+
+			case WM_COMMAND: {
+				switch (LOWORD(wParam)) {
+					case BTN_INGRESAR: {
+						EnableWindow(hMenuPrincial, 1);
+						EndDialog(hwnd, BTN_INGRESAR);
+						break;
+					}
+					case BTN_SALIR: {
+						PostQuitMessage(0);
+						break;
+					}
+				}
+				break;
+			}
+			case	WM_CLOSE: {
+				DestroyWindow(hwnd);
+				break;
+			}
+			case WM_DESTROY: {
+				PostQuitMessage(1);
+				break;
+			}
 
 		}
 		return FALSE;
